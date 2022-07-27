090b49589762b307ecda38a36a5e0de4877cbbc6
HediAtig <Med-Hedi.Ben-Atig@epita.fr>
2022-07-27 03:31:27 +0200

    hedi commit

--- background.js
+++ background.js
@@ -8,18 +8,45 @@ async function getCurrentTab() {
 }
 
 
+
+// url = "https://fnd-api.herokuapp.com/"
+url = "http://127.0.0.1:8000/"
+
 /*chrome.tabs.onActivated.addListener(async function () {
     console.log("TAB CHANGED")
     console.log(await getCurrentTab())
 })*/
 
-chrome.action.onClicked.addListener( async function () {
-    
-    URLL = await getCurrentTab()
-    console.log(URLL)
+
+
+chrome.action.onClicked.addListener(async function () {
+    dataURL = ''
+    let queryOptions = { active: true, currentWindow: true }
+    let [tab] = await chrome.tabs.query(queryOptions)
+    dataURL = tab.url
+
+
+    console.log(dataURL)
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        mode: 'same-origin',
+        body: JSON.stringify({ 'msg': dataURL })
+    }).then(resp => {
+        if (resp.status === 200) {
+            return resp.json()
+        } else {
+            console.log("Status: " + resp.status)
+            return Promise.reject("server")
+        }
+    })
 })
 
 
 
- 
+
 
--- popup.js
+++ popup.js
@@ -1,8 +1,101 @@
-chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-    document.write(`<h3>The tabs you're on are:</h3>`);
-    document.write('<ul>');
-    for (let i = 0; i < tabs.length; i++) {
-      document.write(`<li>${tabs[i].url}</li>`);
+const run = document.querySelector(".run");
+const message = document.querySelector(".message");
+const loading = document.querySelector(".spinner");
+loading.style.display = "none";
+run.style.display = "none";
+
+
+async function getCurrentTabAPI() {
+  dataURL = '';
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  dataURL = tab.url;
+
+  if (checkUrl(dataURL)>0) {
+    post_api(dataURL)
+    setTimeout(get_api, 5000)
+
+  }
+  else{
+    const msjDiv = document.querySelector("#message");
+    const msjElement = document.createElement("p");
+    msjElement.innerText = `Sorry, we do not support this website.`;
+    msjDiv.append(msjElement);
+    process.exit();
+  }
+}
+
+function checkUrl(url) {
+  let websites = ["https://www.huffpost.com/*", "https://www.aljazeera.com/", "https://www.latimes.com/",
+    "https://www.bbc.com/", "https://www.dailymail.co.uk/", "https://www.forbes.com/",
+    "https://www.cnbc.com/", "https://www.nbcnews.com/", "https://www.foxnews.com/",
+    "https://news.yahoo.com/"]
+
+  for (let i = 0; i < websites.length + 1; i++) {
+    if (url.includes(websites[i])) {
+      return i
     }
-    document.write('</ul>');
-  });
+
+  }
+}
+
+  function post_api(web) {
+    loading.style.display = "block";
+    run.style.display = "block";
+
+    fetch("http://127.0.0.1:8000/", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      mode: 'same-origin',
+      body: JSON.stringify({ 'msg': web })
+    }).then(resp => {
+      if (resp.status === 200) {
+        return resp.json()
+      } else {
+        console.log("Status: " + resp.status)
+        return Promise.reject("server")
+      }
+    })
+  }
+
+  function get_api() {
+    fetch("http://127.0.0.1:8000/prediction", {
+    method: 'GET',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    mode: 'same-origin',
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("NETWORK RESPONSE ERROR");
+      }
+    })
+    .then(data => {
+      loading.style.display = "none";
+      run.style.display = "none";
+
+      showData(data.message);
+    })
+    .catch((error) => console.error("FETCH ERROR:", error));
+
+  showData = message => {
+    const msjDiv = document.querySelector("#message");
+    const msjElement = document.createElement("p");
+    msjElement.innerText = `Result: ${message}`;
+    msjDiv.append(msjElement);
+  }
+}
+
+
+getCurrentTabAPI()
+
+
+
+
